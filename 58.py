f81e788ad357b6c16d4c8c707a32cfcfc8b6367a
Hidde Vogelpoel <hidde.vogelpoel@gmail.com>
2019-03-31 19:08:44 +0200

    Opdracht 5.8 gefixt

--- 58.py
+++ 58.py
@@ -4,8 +4,8 @@ getal1 = pcinput.getInteger('Getal1 = ')
 getal2 = pcinput.getInteger('Getal2 = ')
 getal3 = pcinput.getInteger('Getal3 = ')
 som = getal1 + getal2 + getal3
-gemiddelde = float(som/3)
+gemiddelde = som/3
 
 print('Het grootste getal is {}').format(max(getal1, getal2, getal3))
 print('Het kleinste getal is {}').format(min(getal1, getal2, getal3))
-print('Het gemiddelde is {}').format(gemiddelde)
+print('Het gemiddelde is %.2f') % gemiddelde

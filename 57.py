e160c5729564e29d62e00afb01cc6b3d257d4238
Hidde Vogelpoel <hidde.vogelpoel@gmail.com>
2019-04-03 15:43:49 +0200

    Opdracht 57 update

--- 57.py
+++ 57.py
@@ -4,6 +4,6 @@ import math
 zijde1 = pcinput.getFloat('Lengte rechthoekszijde1 = ')
 zijde2 = pcinput.getFloat('Lengte rechthoekszijde1 = ')
 kwadraat = zijde1**2 + zijde2**2
-zijde3 = math.sqrt(kwadraat)
+schuinezijde = math.sqrt(zijde1**2 + zijde2**2)
 
-print('De schuine zijde = %.2f') % zijde3
+print('De schuine zijde = %.2f') % schuinezijde

59fdf6e3eabdb327243b49a2b789f23d132b5c03
Hidde Vogelpoel <hidde.vogelpoel@gmail.com>
2019-04-04 15:12:25 +0200

    Opdracht E6

---
+++ E6.py
@@ -0,0 +1,14 @@
+# Imports
+import pcinput
+
+# Inputs
+oorspronkelijkSaldo = pcinput.getFloat('Geef oorspronkelijk saldo: ')
+rente = pcinput.getFloat('Geef rente: ')
+aantalJaar = pcinput.getFloat('Geef het aantal jaar: ')
+
+# Berekeningen
+groeifactor = 1 + rente/100
+nieuwSaldo = round(oorspronkelijkSaldo * groeifactor**aantalJaar,2)
+
+# Eindresultaat printen
+print('Na {} jaar is je banksaldo {}').format(int(aantalJaar), nieuwSaldo)
